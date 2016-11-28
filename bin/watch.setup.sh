376b1ccac0dcd769de5a9e7cb58713469a9afd7d
Daniel Haus <daniel.haus@businesstools.de>
2016-11-28 22:11:34 +0100

    Ensure file is modified once at most

--- bin/watch.setup.sh
+++ bin/watch.setup.sh
@@ -6,15 +6,17 @@ if [ "$NODE_ENV" = "development" ]; then
     mkdir -p /etc/service/watch
     ln -sf /usr/local/bin/watch.run.sh /etc/service/watch/run
 
-    sed -i '/robots/a\
-        location /__webpack_hmr { \
-            proxy_pass http://127.0.0.1:8080/__webpack_hmr; \
-            proxy_http_version 1.1; \
-            proxy_set_header Upgrade $http_upgrade; \
-            proxy_set_header Connection "upgrade"; \
-        }
-    ' /etc/nginx/sites-available/default
-
+    if [ ! -f /.development ]; then
+        touch /.development
+        sed -i '/robots/a\
+            location /__webpack_hmr { \
+                proxy_pass http://127.0.0.1:8080/__webpack_hmr; \
+                proxy_http_version 1.1; \
+                proxy_set_header Upgrade $http_upgrade; \
+                proxy_set_header Connection "upgrade"; \
+            }
+        ' /etc/nginx/sites-available/default
+    fi
 else
     echo "Running in production mode"
 fi
